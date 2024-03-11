0ffacfb17dcf643006b2458254fd4e033fdaa5ac
josteng <jonas@stenglein.dev>
2024-03-11 12:53:11 +0100

    Downgrade target SDK version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "dev.stenglein.connectivitycontroller"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 28
         versionCode = 1
         versionName = "1.0"
 
